897a6b70a0879473962aa54775ffaec1d00a146a
Colin <cjdunn109@gmail.com>
2019-03-13 18:10:24 -0700

    redux registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -10,21 +12,27 @@ import Login from './components/auth/Login';
 
 import './App.css';
 
+// NOTICE, there is no path for Navbar or Footer, this means they are rendered to the dom in EVERY path
+// This is the same as having a base file and extending like in Django.
+// Use this in the future, really important stuff 
+
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path = "/" component={Landing} />
-          <div className="container">
-            <Route exact path = "/register" component = {Register} />
-            <Route exact path = "/login" component = {Login} />
+      <Provider store={ store }>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path = "/" component={Landing} />
+            <div className="container">
+              <Route exact path = "/register" component = {Register} />
+              <Route exact path = "/login" component = {Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-import axios from 'axios';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
 
 class Register extends Component {
@@ -22,6 +25,12 @@ class Register extends Component {
         });
     }
 
+    componentWillReceiveProps = nextProps => {
+        if(nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+    }
+
     onSubmit = event => {
         event.preventDefault();
 
@@ -32,13 +41,12 @@ class Register extends Component {
             password2: this.state.password2
         }
 
-        axios.post('/api/users/register', newUser)
-            .then(res => console.log(res.data))
-            .catch(err => this.setState({ errors: err.response.data}));
+        this.props.registerUser(newUser, this.props.history); // So we can use this in our actions
     }
 
     render() {
         const errors = this.state.errors;
+
         return (
             <div className="register">
                 <div className="container">
@@ -110,4 +118,15 @@ class Register extends Component {
     }
 }
 
-export default Register;
+Register.propTypes = {
+    registerUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
