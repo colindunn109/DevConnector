0e39ee0e7166cae4aedbb56691cf1ce0ecc5cd51
Colin <cjdunn109@gmail.com>
2019-03-15 00:10:06 -0700

    profile redux

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getCurrentProfile } from '../../actions/profileActions';
 
 
@@ -11,12 +12,54 @@ class Dashboard extends Component {
     }
 
     render() {
+
+        const { user } = this.props.auth;
+        const { profile, loading } = this.props.profile;
+
+        let dashboardContent;
+
+        if(profile === null || loading) {
+            dashboardContent = <h4>Loading...</h4>
+        }
+        else {
+            if(Object.keys(profile).length > 0) { // Profile found
+                dashboardContent = <h4>Display Profile</h4>
+            }
+            else{ // Logged in but no profile
+                dashboardContent = (
+                    <div>
+                        <p className="lead text-muted">Welcome {user.name}</p>
+                        <p>You have not set up a profile, create one and get connected!</p>
+                        <Link to="/create-profile" className="btn btn-lg btn-info">Create Profile</Link>
+                    </div>
+                )
+            }
+        }
+
         return (
-        <div>
-        
+        <div className="dashboard">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4">Dashboard</h1>
+                        {dashboardContent}
+                    </div>
+                </div>
+            </div>
         </div>
         )
     }
 }
 
-export default connect(null, { getCurrentProfile })(Dashboard);
+const mapStateToProps = state => ({
+    profile: state.profile,
+    auth: state.auth
+});
+
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+}
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
